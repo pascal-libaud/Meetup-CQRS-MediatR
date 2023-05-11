e4d9ec071489084f6b40e6504e004258ffe8919f
Thomas Coeffic <thomas.coeffic@viseo.com>
2023-05-11 08:00:26 +0200

    Ajout de pipelines pour logger les performances et gérer le retry avec Polly

---
+++ 2-Blog-CQRS/Pipelines/PerformancePipeline.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using System.Diagnostics;
+
+namespace _2_Blog_CQRS.Pipelines
+{
+    public class PerformancePipeline<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<PerformancePipeline<TRequest, TResponse>> _logger;
+
+        public PerformancePipeline(ILogger<PerformancePipeline<TRequest, TResponse>> logger)
+        {
+            this._logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            try
+            {
+                return await next();
+            }
+            finally
+            {
+
+                stopwatch.Stop();
+                _logger.LogInformation($"Handled {typeof(TRequest).Name} in {stopwatch.ElapsedMilliseconds}ms");
+            }
+        }
+    }
+}
